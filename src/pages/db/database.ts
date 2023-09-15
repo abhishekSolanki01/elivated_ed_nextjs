651fb1d2a485d0e080ac5e75fceaf114a680bbb8
Abhishek <abhishek.solanki1110@gmail.com>
2023-09-15 15:30:09 +0530

    Create database.ts

---
+++ src/pages/db/database.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = `mongodb+srv://abhishek:12345@cluster0.pcghker.mongodb.net/?retryWrites=true&w=majority`
+
+// track the connection
+let isConnected = false;
+export const connectToDataBase = async () => {
+  mongoose.set("strictQuery", true);
+  if (isConnected) {
+    console.log("DB connected already");
+    return;
+  }
+  try {
+    await mongoose.connect(CONNECTION_STRING, {
+      dbName: "courses",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+  } catch (error) {
+    console.log(error);
+  }
+};
