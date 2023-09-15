726948170d1eb3771150e70442f0cc5b0a2e9316
Abhishek <abhishek.solanki1110@gmail.com>
2023-09-15 11:45:56 +0530

    Update login.ts

--- src/pages/api/admin/login.ts
+++ src/pages/api/admin/login.ts
@@ -1,1 +1,20 @@
 
+const jwt = require('jsonwebtoken');
+const Admin = require('../db/admin');
+
+const handler = (req, res) => {
+  try{
+    if(req.method==="POST"){
+      const {username, password} = req.headers;
+      const ifAdminExist = await Admin.exists({ username, password });
+      if(ifAdminExist){
+        const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '100h' });
+        res.json({ message: 'Logged in successfully', token, email: username  });
+      }else{
+        res.status(403).json({ message: 'Invalid username or password' });
+      }
+    }
+  }catch(error){
+    
+  }
+}
