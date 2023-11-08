61c101c5872c3ea4bd22e5c66ba5125f073e9a24
Abhishek <abhishek.solanki1110@gmail.com>
2023-11-08 16:20:28 +0530

    Update purchasedCourses.page.ts

--- src/pages/api/users/purchasedCourses.page.ts
+++ src/pages/api/users/purchasedCourses.page.ts
@@ -1,9 +1,13 @@
 import withMiddleware from "@/middleware";
 import { User } from "../../../../db/models/user";
+import { connectToDataBase } from "../../../../db/database";
+
 
 const handler = async(req: any, res: any)=>{
     try {
         if(req.method === "GET"){
+            await connectToDataBase()
+
             const user = await User.findOne({ username: req.user.username }).populate('purchasedCourses');
             if(user){
               res.json({ purchasedCourses: user.purchasedCourses || [] });
